41aad0034e7904358570ffef2a0dfadb13217cc6
Jae-Holderby <jae.holderby@gmail.com>
2017-09-21 11:04:18 -0600

    hashed passwords added

--- seeds/02_instructors.js
+++ seeds/02_instructors.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 exports.seed = function(knex, Promise) {
   return knex.raw('DELETE FROM "instructor"; ALTER SEQUENCE instructor_id_seq RESTART WITH 3;')
 
   .then(() => {
+      var hash = bcrypt.hashSync(process.env.SECRET_WORD, saltRounds)
+      var hash2 = bcrypt.hashSync(process.env.SECRET_WORD2, saltRounds)
       var instructors = [{
         id: 1,
-        name: "Berto",
-        password: "smack"
+        name: "Berto Ortega",
+        password: hash
 
       }, {
         id: 2,
-        name: "Matt",
-        password: "smack"
+        name: "Matt Winzer",
+        password: hash2
       }];
 
       return knex('instructor').insert(instructors);
